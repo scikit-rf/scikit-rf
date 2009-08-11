44f05c558e6c01f375261fc13262ef2d9005057a
alex arsenovic <arsenovic@virginia.edu>
2009-08-11 15:45:46 +0000

    added examples directory with  examples, duh.

---
+++ examples/example1.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import sys
+sys.path.append('../')
+import mwavepy as m 
+import pylab as p 
+#normally mwavepy would be placed in your path, so you can use import instead of __import__
+
+# load the touchone file, which we got from the VNA
+horn = m.loadTouchtone('horn.s2p')
+
+#plot returnloss in dB, on a smith chart and, plot SWR.
+p.figure(1)
+horn.plotReturnLoss()
+
+p.figure(2)
+horn.s11.plotSmith()
+p.axis([-1,1,-1,1])
+
+p.figure(3)
+horn.plotSwr1()
+p.ylim(0,3)
+p.show()
---
+++ examples/readme.txt
@@ -0,0 +1,5 @@
+just run the examples like, 
+	python example1.py 
+and you should see the output shown the in png's. 
+
+you need to have matplotlib(aka pylab), scipy, numpy installed. 
