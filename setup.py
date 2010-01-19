7b002f52b1c09e483cb64edb786c5fc3b004e09d
alex arsenovic <arsenovic@virginia.edu>
2010-01-19 20:10:34 +0000

    added installer

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='mwavepy',
+	version='0.35',
+	description='Microwave Engineering Functions for python',
+	author='Alex Arsenovic',
+	author_email='arsenovic@virginia.edu',
+	url='http://code.google.com/p/mwavepy/',
+	packages=['mwavepy','mwavepy.pythics_instruments'])
