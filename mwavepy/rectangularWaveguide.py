ffcb2181fe6283fe645fe56a0b5f20051389436e
alex arsenovic <arsenovic@virginia.edu>
2010-07-23 21:03:51 +0000

    starting writing the transverse mdoe functions

--- mwavepy/rectangularWaveguide.py
+++ mwavepy/rectangularWaveguide.py
@@ -21,7 +21,7 @@
 
 from scipy.constants import  epsilon_0, mu_0, c,pi, mil,pi
 import numpy as npy
-from numpy import sqrt,array
+from numpy import sqrt,array, cos, sin, linspace
 # would there be any benefit to structuring modes as objects?
 #def RectangularWaveguideMode(object,m,n):
 	#self.m = m
@@ -33,19 +33,27 @@ from numpy import sqrt,array
 			#sqrt( (self.m/a)**2 + (self.n/2)**2)
 import pdb		
 class RectangularWaveguide(object):
+	'''
+	represents a homogeneously rectangular waveguide.
+	
+	cross-section is axb, and it is homogeneously with relative
+	permativity and permiabilty, epsilon_R and mu_R respectivly
+	'''
 	def __init__(self, a,b=None,epsilon_R=1, mu_R=1):
 		self.a = a
 		if b is None: 
 			self.b = a/2.
 		else:
 			self.b = b
+			
 		self.epsilon = epsilon_R * epsilon_0
 		self.mu = mu_R*mu_0
 		
+		# link function names for convinience
 		self.z0 = self.characteristic_impedance
 		self.y0 = self.characteristic_admittance
-		self.lambdac = self.cutoff_wavelength
-		self.fc = self.cutoff_frequency
+		self.lambda_c = self.cutoff_wavelength
+		self.f_c = self.cutoff_frequency
 		self.gamma = self.kz
 		
 	## frequency independent functions
@@ -147,5 +155,37 @@ class RectangularWaveguide(object):
 		'''
 		return 1./(self.characteristic_impedance(mode_type,m,n,f))
 	
-	#def e_t(self,xy):
+	def e_t(self,mode_type, m,n, x_points=201, y_points = 101):
+		'''
+		discretized transverse mode functions for the electric field. 
 		
+		usable for numerical evaluation of eigen-space, or field visualization
+		'''
+		a,b,kx,ky= self.a,self.b,self.kx(m), self.ky(n)
+		#pdb.set_trace()
+		x_vector= linspace(0,a,x_points)
+		y_vector= linspace(0,b,y_points)
+		x,y = npy.meshgrid(x_vector, y_vector)
+		
+		## TM
+		if mode_type == 'tmz' or mode_type == 'tm':
+			common_factor = 1/sqrt(m**2 *(b/a)+ n**2 *(a/b))
+			e_t  = [\
+				-2*m/a * common_factor * cos(kx*x)*sin(ky*y),\
+				-2*n/b * common_factor * sin(kx*x)*cos(ky*y),\
+				-1j/a*b * 1/common_factor * sin(kx*x)*sin(ky*y)\
+				]
+		## TE
+		elif  mode_type == 'tez' or mode_type == 'te':
+			# nuemann numbers
+			ep_m = 2.-1*(m==0)
+			ep_n = 2.-1*(n==0)
+			common_factor = ep_m*ep_n/sqrt(m**2 *(b/a)+ n**2 *(a/b))
+			
+			e_t  = [\
+				n/b *  common_factor * 	cos(kx*x)*sin(ky*y),\
+				-m/a *  common_factor * sin(kx*x)*cos(ky*y),\
+				0\
+				]	
+				
+		return array(e_t)
