a23deba42c352402ed36d9bcf34b63d32eb18b1f
alex arsenovic <arsenovic@virginia.edu>
2010-09-28 16:38:46 +0000

    fixed error in setup and added teh zip source disto

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 setup(name='mwavepy',
 	version='1.0',
 	description='Microwave Engineering Functions for python',
-	author=['Alex Arsenovic','Lihan Chen'],
+	author='Alex Arsenovic, Lihan Chen',
 	author_email='arsenovic@virginia.edu',
 	url='http://code.google.com/p/mwavepy/',
 	packages=['mwavepy','mwavepy.transmissionLine','mwavepy.virtualInstruments'])
