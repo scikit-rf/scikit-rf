2ea004c5b0ee1c5b110800858874267ff0568321
alex arsenovic <arsenovic@virginia.edu>
2010-11-17 14:57:02 +0000

    added testCases

---
+++ mwavepy/testCases/testCalibration.py
@@ -0,0 +1,70 @@
+import unittest
+import mwavepy as mv
+
+
+
+class OnePortStandardCalibration(unittest.TestCase):
+	'''
+	One-port calibration test.
+
+	loads data for a reciprocal embeding network, and some ideal
+	standards. ficticous measurements are made by cascading the ideals
+	behind teh embeding network, and a calibration is performed.
+
+	the calculated embedding network and de-embeded ideals are compared
+	to originals as a metric of 'working'
+	
+	'''
+	def setUp(self):
+		self.short = mv.Network('short.s1p')
+		self.match = mv.Network('match.s1p')
+		self.open = mv.Network('open.s1p')
+		self.delay_short = mv.Network('delay short.s1p')
+		self.embeding_network = mv.Network('embedingNetwork.s2p')
+		
+
+	def test_standard_calibration(self):
+		ideals, measured = [],[]
+		std_list = [self.short, self.match,self.open] 
+
+		for ntwk in std_list:
+			ideals.append(ntwk)
+			measured.append(self.embeding_network**ntwk)
+
+		cal = mv.Calibration(\
+			ideals = ideals,\
+			measured = measured,\
+			type = 'one port',\
+			frequency = self.short.frequency,\
+			is_reciprocal = True,\
+			)
+		# did we find correct embeding network?
+		self.assertEqual(self.embeding_network, cal.error_ntwk)
+		# are the de-embeded networks the same as their ideals?
+		for ntwk in std_list:
+			self.assertEqual(ntwk,  cal.apply_cal(self.embeding_network**ntwk))
+
+	def test_least_squares_calibration(self):
+		ideals, measured = [],[]
+		std_list = [self.short, self.match,self.open,self.delay_short] 
+
+		for ntwk in std_list:
+			ideals.append(ntwk)
+			measured.append(self.embeding_network**ntwk)
+
+		cal = mv.Calibration(\
+			ideals = ideals,\
+			measured = measured,\
+			type = 'one port',\
+			frequency = self.short.frequency,\
+			is_reciprocal = True,\
+			)
+		# did we find correct embeding network?
+		self.assertEqual(self.embeding_network, cal.error_ntwk)
+		# are the de-embeded networks the same as their ideals?
+		for ntwk in std_list:
+			self.assertEqual(ntwk,  cal.apply_cal(self.embeding_network**ntwk))
+
+	
+if __name__ == "__main__":
+   unittest.main()
---
+++ mwavepy/testCases/testNetwork.py
@@ -0,0 +1,27 @@
+import unittest
+import mwavepy as mv
+
+
+
+class NetworkOperationsTestCase(unittest.TestCase):
+	'''
+	Network class operation test case. 
+	
+	 As tested by lihan in ADS the following is true:
+		test 3 == test1 ** test2
+	
+	'''
+	def setUp(self):
+		self.ntwk1 =mv.Network('ntwk1.s2p')
+		self.ntwk2 =mv.Network('ntwk2.s2p')
+		self.ntwk3 =mv.Network('ntwk3.s2p')
+
+	def test_cascade(self):
+		self.assertEqual(self.ntwk1**self.ntwk2, self.ntwk3)
+
+	def test_de_embed_by_inv(self):
+		self.assertEqual(self.ntwk1.inv**self.ntwk3,self.ntwk2)
+		self.assertEqual(self.ntwk3**self.ntwk2.inv,self.ntwk1)
+
+if __name__ == "__main__":
+   unittest.main()
