c436bda554b80f7946e385d5e716f5eccd2e20a8
alex arsenovic <arsenovic@virginia.edu>
2010-12-02 19:14:24 +0000

    modified example

--- examples/vnaNoiseAnalysis/recordTouchstoneFiles.py
+++ examples/vnaNoiseAnalysis/recordTouchstoneFiles.py
@@ -11,13 +11,15 @@ import mwavepy as mv
 import os,datetime
 
 nsweeps = 101 # number of sweeps to take
-dir = 'touchstoneFiles2' # directory to save files in 
+dir = datetime.datetime.now().date().__str__() # directory to save files in 
 
 myvna = mv.vna.HP8720() # HP8510 also available
-
+os.mkdir(dir)
 for k in range(nsweeps):
 	print  k 
 	ntwk = myvna.s11
 	date_string = datetime.datetime.now().__str__().replace(':','-')
 	ntwk.write_touchstone(dir +'/'+ date_string)
 	
+myvna.close()
+
