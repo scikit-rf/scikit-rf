939f35a8927c7ffdd68ea922b89fae8fab14c7af
alex arsenovic <arsenovic@virginia.edu>
2012-03-27 09:06:31 -0400

    docs: minor works
    added the scikit-rf-title to the manual cover,
    added a link to the pdf to the html doc index.
    edited the installation a tad to point to new site

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -193,7 +193,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = '_static/scikit-rf-title.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
