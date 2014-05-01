875c56da5cec4472d156440d30ea5acdd33edfb8
alex arsenovic <arsenovic@virginia.edu>
2014-05-01 15:48:54 -0400

    moded conf to allow build off git

--- doc/source/conf-bootstrap.py
+++ doc/source/conf-bootstrap.py
@@ -30,7 +30,7 @@ import sphinx_bootstrap_theme
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.insert(0, os.path.abspath('../sphinxext'))
-sys.path.insert(0, os.path.abspath('../../build/lib.linux-x86_64-2.7/'))
+#sys.path.insert(0, os.path.abspath('../../build/lib.linux-x86_64-2.7/'))
 
 import skrf 
 print skrf 
