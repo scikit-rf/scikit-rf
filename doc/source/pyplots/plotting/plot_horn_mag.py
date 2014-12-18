0f8ee6ccc449475342f05fc88231ef619f12a3b7
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-12-18 15:53:43 +0100

    typo: indecies -> indices

--- doc/source/pyplots/plotting/plot_horn_mag.py
+++ doc/source/pyplots/plotting/plot_horn_mag.py
@@ -7,7 +7,7 @@ horn = rf.Network('horn antenna.s1p')
 # plot magnitude (in db) of S11
 pylab.figure(1)
 pylab.title('Return Loss (Mag)')
-horn.plot_s_db(m=0,n=0) # m,n are S-Matrix indecies
+horn.plot_s_db(m=0,n=0) # m,n are S-Matrix indices
 
 
 # show the plots
--- examples/basic_touchstone_plotting/plot_ringslot_mag.py
+++ examples/basic_touchstone_plotting/plot_ringslot_mag.py
@@ -7,8 +7,8 @@ ring_slot= rf.Network('ring slot array measured.s1p')
 # plot magnitude (in db) of S11
 pylab.figure(1)
 pylab.title('WR-10 Ringslot Array, Mag')
-ring_slot.plot_s_db(m=0,n=0) # m,n are S-Matrix indecies
+ring_slot.plot_s_db(m=0,n=0) # m,n are S-Matrix indices
 pylab.figure(2)
-ring_slot.plot_s_db(m=0,n=0) # m,n are S-Matrix indecies
+ring_slot.plot_s_db(m=0,n=0) # m,n are S-Matrix indices
 # show the plots
 pylab.show()
