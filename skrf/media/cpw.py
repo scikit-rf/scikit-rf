5f2656d85991ab15e098391d578448cf9f050ba1
Wylie Standage-Beier <thewyliestcoyote@gmail.com>
2015-02-07 13:46:02 -0700

    Improve code style skrf/media/cpw.py

--- skrf/media/cpw.py
+++ skrf/media/cpw.py
@@ -10,7 +10,7 @@ contains CPW class
 '''
 from scipy.constants import  epsilon_0, mu_0
 from scipy.special import ellipk
-from numpy import real, imag,pi,sqrt,log,zeros, ones
+from numpy import real, imag, pi, sqrt, log, zeros, ones
 from .media import Media
 from ..tlineFunctions import skin_depth, surface_resistivity
 
@@ -27,7 +27,7 @@ class CPW(Media):
     .. [#] http://qucs.sourceforge.net/docs/technical.pdf
     .. [#] http://www.qucs.sourceforge.net/
     '''
-    def __init__(self, frequency, w , s, ep_r, t=None, rho=None, \
+    def __init__(self, frequency, w, s, ep_r, t=None, rho=None,\
             *args, **kwargs):
         '''
         Coplanar Waveguide initializer
@@ -46,28 +46,26 @@ class CPW(Media):
                 conductor thickness, in m.
         rho: number, or array-like, optional
                 resistivity of conductor (None)
-
-
         '''
         self.frequency, self.w, self.s, self.ep_r, self.t, self.rho =\
                 frequency, w, s, ep_r, t, rho
-
-        Media.__init__(self,\
-                frequency = frequency,\
-                propagation_constant = self.gamma, \
-                characteristic_impedance = self.Z0,\
-                *args, **kwargs)
+        Media.__init__(self,
+                       frequency=frequency,
+                       propagation_constant=self.gamma,
+                       characteristic_impedance=self.Z0,
+                       *args, **kwargs)
 
     def __str__(self):
-        f=self.frequency
-        output =  \
-                'Coplanar Waveguide Media.  %i-%i %s.  %i points'%\
-                (f.f_scaled[0],f.f_scaled[-1],f.unit, f.npoints) + \
-                '\n W= %.2em, S= %.2em'% \
-                (self.w,self.s)
+
+        f = self.frequency
+        output = 'Coplanar Waveguide Media.  %i-%i %s.  %i points'%\
+                 (f.f_scaled[0], f.f_scaled[-1], f.unit, f.npoints) + \
+                 '\n W= %.2em, S= %.2em'% \
+                 (self.w, self.s)
         return output
 
     def __repr__(self):
+
         return self.__str__()
 
     @property
@@ -90,14 +88,10 @@ class CPW(Media):
         intermediary parameter. see qucs docs on cpw lines.
         '''
         k1 = self.k1
-
         if (0 <= k1 <= 1/sqrt(2)):
-            return pi/(log(2*(1+sqrt(k1))/(1-sqrt(k1)) ))
+            return pi/(log(2*(1+sqrt(k1))/(1-sqrt(k1))))
         elif (1/sqrt(2) < k1 <= 1):
-            return (log(2*(1+sqrt(k1))/(1-sqrt(k1)) ))/pi
-
-
-
+            return (log(2*(1+sqrt(k1))/(1-sqrt(k1))))/pi
 
     @property
     def alpha_conductor(self):
@@ -114,33 +108,27 @@ class CPW(Media):
         '''
         if self.rho is None or self.t is None:
             raise(AttributeError('must provide values conductivity and conductor thickness to calculate this. see initializer help'))
-
-        t, k1, ep_re = self.t, self.k1,self.ep_re
+        t, k1, ep_re = self.t, self.k1, self.ep_re
         r_s = surface_resistivity(f=self.frequency.f, rho=self.rho, \
                 mu_r=1)
         a = self.w/2.
         b = self.s+self.w/2.
         K = ellipk      # complete elliptical integral of first kind
         K_p = lambda x: ellipk(sqrt(1-x**2)) # ellipk's compliment
-
-        return ((r_s * sqrt(ep_re)/(480*pi*K(k1)*K_p(k1)*(1-k1**2) ))*\
-                (1./a * (pi+log((8*pi*a*(1-k1))/(t*(1+k1)))) +\
-                 1./b * (pi+log((8*pi*b*(1-k1))/(t*(1+k1))))))
-
-
-
+        return ((r_s*sqrt(ep_re)/(480*pi*K(k1)*K_p(k1)*(1-k1**2)))*\
+                (1./a*(pi+log((8*pi*a*(1-k1))/(t*(1+k1))))+\
+                 1./b*(pi+log((8*pi*b*(1-k1))/(t*(1+k1))))))
 
     def Z0(self):
         '''
         Characterisitc impedance
         '''
-        return (30.*pi / sqrt(self.ep_re) * self.K_ratio)*ones(len(self.frequency.f), dtype='complex')
-
+        return (30.*pi / sqrt(self.ep_re) * self.K_ratio)*\
+               ones(len(self.frequency.f), dtype='complex')
 
     def gamma(self):
         '''
         Propagation constant
-
 
         See Also
         --------
@@ -150,5 +138,4 @@ class CPW(Media):
         alpha = zeros(len(beta))
         if self.rho is not None and self.t is not None:
             alpha = self.alpha_conductor
-
         return beta+alpha
