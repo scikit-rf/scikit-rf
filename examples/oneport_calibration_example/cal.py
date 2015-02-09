dc963c718f2975627b262f595a8f2c7df5c6a05a
Wylie Standage-Beier <thewyliestcoyote@gmail.com>
2015-02-08 20:17:39 -0700

    Improve code style examples/oneport_calibration_example/cal.py

--- examples/oneport_calibration_example/cal.py
+++ examples/oneport_calibration_example/cal.py
@@ -1,20 +1,18 @@
+
 import skrf as rf
 from pylab import *
 
-cal = rf.Calibration(\
-        measured = [\
-                rf.Network('measured/short.s1p'),
-                rf.Network('measured/delay short 132um.s1p'),
-                rf.Network('measured/delay short 85um.s1p'),
-                rf.Network('measured/load.s1p'),
-                ],
-        ideals =[\
-                rf.Network('ideals/short.s1p'),
-                rf.Network('ideals/delay short 132um.s1p'),
-                rf.Network('ideals/delay short 85um.s1p'),
-                rf.Network('ideals/load.s1p'),
-                ],
-        )
+cal = rf.Calibration(measured=[rf.Network('measured/short.s1p'),
+                               rf.Network('measured/delay short 132um.s1p'),
+                               rf.Network('measured/delay short 85um.s1p'),
+                               rf.Network('measured/load.s1p'),
+                              ],
+                     ideals =[rf.Network('ideals/short.s1p'),
+                              rf.Network('ideals/delay short 132um.s1p'),
+                              rf.Network('ideals/delay short 85um.s1p'),
+                              rf.Network('ideals/load.s1p'),
+                             ],
+                    )
 
 ro_meas = rf.Network('dut/radiating open.s1p')
 ro_cal = cal.apply_cal(ro_meas)
@@ -25,4 +23,5 @@ figure()
 ro_cal.plot_s_db(label='Experiment')
 ro_sim.plot_s_db(label='Simulated')
 
-draw();show();
+draw()
+show()
