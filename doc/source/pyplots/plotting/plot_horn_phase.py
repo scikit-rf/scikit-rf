a12c4d3688d3ee541b7c32ab88de65c3fa8800ad
Wylie Standage-Beier <thewyliestcoyote@gmail.com>
2015-02-08 18:13:07 -0700

    Improve code style doc/source/pyplots/plotting/*.py

--- doc/source/pyplots/plotting/plot_horn_phase.py
+++ doc/source/pyplots/plotting/plot_horn_phase.py
@@ -1,8 +1,10 @@
+
 import pylab
 import skrf as rf
 
 horn = rf.Network('horn antenna.s1p')
 pylab.title('WR-10 Horn Antenna')
-horn.plot_s_deg(m=0,n=0,color='r',marker='o',markevery=5)
+horn.plot_s_deg(m=0, n=0, color='r', marker='o',markevery=5)
 # kwargs are passed through to pylab.plot()
+
 pylab.show()
