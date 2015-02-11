7c0ae3a0c69d57678dce4f2903b085cd0f2f648e
Wylie Standage-Beier <thewyliestcoyote@gmail.com>
2015-02-10 20:38:10 -0700

    Remove trailing whitespace

--- skrf/tests/test_network.py
+++ skrf/tests/test_network.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import numpy as npy
-import cPickle as pickle 
+import cPickle as pickle
 import skrf as rf
 from nose.plugins.skip import SkipTest
 class NetworkTestCase(unittest.TestCase):
@@ -20,26 +20,26 @@ class NetworkTestCase(unittest.TestCase):
         self.ntwk1 = rf.Network(os.path.join(self.test_dir, 'ntwk1.s2p'))
         self.ntwk2 = rf.Network(os.path.join(self.test_dir, 'ntwk2.s2p'))
         self.ntwk3 = rf.Network(os.path.join(self.test_dir, 'ntwk3.s2p'))
-    
+
     def test_constructor_empty(self):
         rf.Network()
-    
+
     def test_constructor_from_values(self):
         rf.Network(f=[1,2],s=[1,2],z0=[1,2] )
-    
+
     def test_constructor_from_touchstone(self):
         rf.Network(os.path.join(self.test_dir, 'ntwk1.s2p'))
-    
+
     def test_constructor_from_pickle(self):
         rf.Network(os.path.join(self.test_dir, 'ntwk1.ntwk'))
-        
-    
+
+
     def test_open_saved_touchstone(self):
         self.ntwk1.write_touchstone('ntwk1Saved',dir=self.test_dir)
         ntwk1Saved = rf.Network(os.path.join(self.test_dir, 'ntwk1Saved.s2p'))
         self.assertEqual(self.ntwk1, ntwk1Saved)
         os.remove(os.path.join(self.test_dir, 'ntwk1Saved.s2p'))
-    
+
     def test_pickling(self):
         original_ntwk = self.ntwk1
         f = open(os.path.join(self.test_dir, 'pickled_ntwk.ntwk'),'wb')
@@ -50,12 +50,12 @@ class NetworkTestCase(unittest.TestCase):
         self.assertEqual(original_ntwk, unpickled)
         f.close()
         os.remove(os.path.join(self.test_dir, 'pickled_ntwk.ntwk'))
-            
+
     def test_stitch(self):
         tmp = self.ntwk1.copy()
         tmp.f = tmp.f+ tmp.f[0]
         c = rf.stitch(self.ntwk1, tmp)
-        
+
     def test_cascade(self):
         self.assertEqual(self.ntwk1 ** self.ntwk2, self.ntwk3)
 
@@ -69,7 +69,7 @@ class NetworkTestCase(unittest.TestCase):
         xformer.z0 = (50,25)  # transforms 50 ohm to 25 ohm
         c = rf.connect(xformer,0,xformer,1)  # connect 50 ohm port to 25 ohm port
         self.assertTrue(npy.all(npy.abs(c.s-rf.impedance_mismatch(50, 25)) < 1e-6))
-    
+
     def test_connect_multiports(self):
         a = rf.Network()
         a.frequency=(1,)
@@ -94,7 +94,7 @@ class NetworkTestCase(unittest.TestCase):
         xformer.z0 = (50,25)  # transforms 50 ohm to 25 ohm
         c = rf.connect_fast(xformer,0,xformer,1)  # connect 50 ohm port to 25 ohm port
         self.assertTrue(npy.all(npy.abs(c.s-rf.impedance_mismatch(50, 25)) < 1e-6))
-    
+
     def test_flip(self):
         self.assertEqual(rf.connect(self.ntwk1, 1, self.ntwk2, 0) , \
             self.ntwk3)
@@ -129,17 +129,17 @@ class NetworkTestCase(unittest.TestCase):
 
     def test_plot_two_port_smith(self):
         self.ntwk1.plot_s_smith()
-        
+
     def test_zy_singularities(self):
         open = rf.N(f=[1], s=[1], z0=[50])
         short = rf.N(f=[1], s=[-1], z0=[50])
         react = rf.N(f=[1],s=[[0,1],[1,0]],z0=50)
-        z = open.z 
+        z = open.z
         y = short.y
         a = react.y
-        
-        
-        
+
+
+
     def test_yz(self):
         tinyfloat = 1e-12
         ntwk = rf.Network()
@@ -151,7 +151,7 @@ class NetworkTestCase(unittest.TestCase):
         self.assertTrue((abs(rf.y2s(ntwk.y, ntwk.z0)-ntwk.s) < tinyfloat).all())
         self.assertTrue((abs(rf.z2y(ntwk.z)-ntwk.y) < tinyfloat).all())
         self.assertTrue((abs(rf.z2s(ntwk.z, ntwk.z0)-ntwk.s) < tinyfloat).all())
-    
+
     def test_mul(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         # operating on  networks
@@ -161,7 +161,7 @@ class NetworkTestCase(unittest.TestCase):
         # operating on list
         self.assertTrue( ((a*[1,2]).s == npy.array([[[1+2j]],[[6+8j]]])).all())
         self.assertTrue( (([1,2]*a).s == npy.array([[[1+2j]],[[6+8j]]])).all())
-        
+
     def test_sub(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         # operating on  networks
@@ -169,8 +169,8 @@ class NetworkTestCase(unittest.TestCase):
         # operating on numbers
         self.assertTrue( ((a-(2+2j)).s == npy.array([[[-1+0j]],[[1+2j]]])).all())
         # operating on list
-        self.assertTrue( ((a-[1+1j,2+2j]).s == npy.array([[[0+1j]],[[1+2j]]])).all()) 
-    
+        self.assertTrue( ((a-[1+1j,2+2j]).s == npy.array([[[0+1j]],[[1+2j]]])).all())
+
     def test_div(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         # operating on  networks
@@ -178,8 +178,8 @@ class NetworkTestCase(unittest.TestCase):
         # operating on numbers
         self.assertTrue( ((a/2.).s == npy.array([[[.5+1j]],[[3/2.+2j]]])).all())
         # operating on list
-        self.assertTrue( ((a/[1,2]).s == npy.array([[[1+2j]],[[3/2.+2j]]])).all())    
-    
+        self.assertTrue( ((a/[1,2]).s == npy.array([[[1+2j]],[[3/2.+2j]]])).all())
+
     def test_add(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         # operating on  networks
@@ -188,31 +188,31 @@ class NetworkTestCase(unittest.TestCase):
         self.assertTrue( ((a+2+2j).s == npy.array([[[3+4j]],[[5+6j]]])).all())
         # operating on list
         self.assertTrue( ((a+[1+1j,2+2j]).s == npy.array([[[2+3j]],[[5+6j]]])).all())
-    
+
     def test_interpolate(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         freq = rf.F.from_f(npy.linspace(1,2,4), unit='ghz')
         b = a.interpolate(freq)
         # TODO: numerically test for correct interpolation
-    
+
     def test_interpolate_self_npoints(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         a.interpolate_self_npoints(4)
         # TODO: numerically test for correct interpolation
-        
+
     def test_interpolate_from_f(self):
         a = rf.N(f=[1,2],s=[1+2j, 3+4j],z0=1)
         a.interpolate_from_f(npy.linspace(1,2,4), unit='ghz')
-        # TODO: numerically test for correct interpolation   
-    
+        # TODO: numerically test for correct interpolation
+
     def test_slicer(self):
         a = rf.Network(f=[1,2,4,5,6],
                        s=[1,1,1,1,1],
                        z0=50 )
-                       
+
         b = a['2-5ghz']
         tinyfloat = 1e-12
         self.assertTrue((abs(b.frequency.f - [2e9,4e9,5e9]) < tinyfloat).all())
-        
+
 suite = unittest.TestLoader().loadTestsFromTestCase(NetworkTestCase)
 unittest.TextTestRunner(verbosity=2).run(suite)
