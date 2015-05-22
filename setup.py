bc1d4adeb2bf5eef2dc579a7fa24b95ef60ad16d
arsenovic <alexanderarsenovic@gmail.com>
2015-05-22 09:41:13 -0400

    making a  quick release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-VERSION = '0.15dev'
+VERSION = '0.14.1'
 LONG_DESCRIPTION = """
 	sckit-rf is an open source approach to RF/Microwave engineering implemented in the Python programming language.
 """
