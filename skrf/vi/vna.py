f9b46b1141b1c50788e6699bb4e1a612079e568c
arsenovic <alexanderarsenovic@gmail.com>
2017-02-17 11:23:09 -0500

    added docs on newvna module

--- skrf/vi/vna.py
+++ skrf/vi/vna.py
@@ -2,14 +2,32 @@
 
 '''
 .. module:: skrf.vi.vna
-=================================================================
+++++++++++++++++++++++++++++++++++++++++++++++++++++
 Vector Network Analyzers (:mod:`skrf.vi.vna`)
-=================================================================
+++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 .. warning::
 
-    These Virtual Instruments are very spotily written, and may be subject
-    to major re-writing in the future.
+    As of 2017.02 a new architecture for vna drivers is being implemented.
+
+New VNA drivers 
+---------------
+
+- VNA drivers will now have a common high level functionality across all vendors implemented in an ABCVNA class.
+- Different vendor drivers will implement their own mid level functionality as needed to implement the ABC class
+- The low level functions are all implemented as SCPI commands which have a new way of being generated and called
+
+Legacy vna module
+------------------------
+The old vna.py module containing drivers for PNA, PNA-X, HP8510, etc. will be available as vna_old.py and can be used as
+follows:
+
+::
+
+    from skrf.vi.vna_old import PNA
+
+Available VNAs
+------------------
 
 .. autosummary::
     :toctree: generated/
