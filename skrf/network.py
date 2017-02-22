ac4bace2655092a40578a27b5380740a333ba539
Julien Hillairet <julien.hillairet@cea.fr>
2017-02-22 12:15:14 +0100

    Passing the path instead of pid solves python 36 bug

--- skrf/network.py
+++ skrf/network.py
@@ -386,8 +386,7 @@ class Network(object):
                 # if unpickling doesn't work then, close fid, reopen in
                 # non-binary mode and try to read it as touchstone
                 fid.close()
-                fid = get_fid(file)
-                self.read_touchstone(fid)
+                self.read_touchstone(file)
 
             if name is None and isinstance(file,str):
                 name = os.path.splitext(os.path.basename(file))[0]
