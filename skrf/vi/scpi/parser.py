a91c6a03556b617c04e95512092f8e931ab7298f
dvincentwest <dvincentwest@gmail.com>
2017-10-30 13:36:25 -0500

    added unicde error handling for parser

--- skrf/vi/scpi/parser.py
+++ skrf/vi/scpi/parser.py
@@ -297,8 +297,13 @@ def parse_yaml_file(driver_yaml_file):
     driver = os.path.splitext(driver_yaml_file)[0] + ".py"
 
     driver_template = None
-    with open(driver_yaml_file, 'r') as yaml_file:
-        driver_template = yaml.load(yaml_file)
+    try:
+        with open(driver_yaml_file, 'r') as yaml_file:
+            driver_template = yaml.load(yaml_file)
+    except UnicodeDecodeError:
+        with open(driver_yaml_file, 'r', encoding='utf8') as yaml_file:
+            driver_template = yaml.load(yaml_file)
+
     sets, queries = parse_branch(driver_template["COMMAND_TREE"])
 
     driver_str = "\n\n\n".join((header_string, string_converter, scpi_preprocessor, query_processor)) + "\n\n\n"
