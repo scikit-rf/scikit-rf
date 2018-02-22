f27823051c52b79e7a80df5ab7ff97e63e8f51b7
arsenovic <alexanderarsenovic@gmail.com>
2018-02-22 14:53:31 -0500

    fix install docs conda line

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(name='scikit-rf',
 		],
 	#ext_modules=[Extension('skrf.src.connect', ['skrf/src/connect.c', ], export_symbols=['innerconnect_s','connect_s'])],
 	package_dir={'skrf':'skrf'},
-	include_package_data = True,
-    data_files = [("", ["LICENSE.txt"])],
+	#include_package_data = True,
+    #data_files = [("", ["LICENSE.txt"])],
 	#exclude_package_data = {'':'doc/*'},
 
 	#package_data = {'skrf':['*tests*']}
