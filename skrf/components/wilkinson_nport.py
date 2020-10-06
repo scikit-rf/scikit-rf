67b4879f1fc137b54ab1a72a4e0fc157fde446f1
maurio <maurio.grando@gmail.com>
2020-10-06 16:12:09 -0700

    updated Ipython notebook thermal example to add worked examples from garcia-garcia and fixed some errors in the n port Wilkinson rf component code.

--- skrf/components/wilkinson_nport.py
+++ skrf/components/wilkinson_nport.py
@@ -9,7 +9,7 @@ class Wilkinson_Nport(Network):
     def __init__(self, N_outs, loss_db = 0, phases = [], name=None, comments=None, f_unit=None, T0=None, s_def='power', **kwargs):
         super().__init__(name=name, comments=comments, f_unit=f_unit, s_def=s_def, **kwargs)
 
-        if loss_db <= 0:
+        if loss_db < 0:
             raise ValueError("loss_db must be a positive value")
 
         if not phases:
@@ -22,10 +22,10 @@ class Wilkinson_Nport(Network):
         
         alpha = (1/npy.sqrt(N_outs))
 
-        nway_mat = npy.zeros((len(self.frequency,N_outs+1, N_outs+1))) 
+        nway_mat = npy.zeros((len(self.frequency),N_outs+1, N_outs+1)) 
 
-        nway_mat[:,0,:] = self._loss * alpha * npy.ones((len(self.frequency,N_outs+1)))
-        nway_mat[:,:,0] = self._loss * alpha * npy.ones((len(self.frequency,N_outs+1)))
+        nway_mat[:,0,1:] = self._loss * alpha * npy.ones((len(self.frequency),N_outs))
+        nway_mat[:,1:,0] = self._loss * alpha * npy.ones((len(self.frequency),N_outs))
 
         self.s = nway_mat
         self.noise_source('passive', T0)
