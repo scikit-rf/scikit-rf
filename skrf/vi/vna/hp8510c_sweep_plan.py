1af6c27373f807442b13efb88b5b9ed4b29577bf
Jonathan deWerd <jjoonathan@gmail.com>
2022-05-21 14:11:26 -0400

    Remove incompatible fstring for py3.7 compat

--- skrf/vi/vna/hp8510c_sweep_plan.py
+++ skrf/vi/vna/hp8510c_sweep_plan.py
@@ -74,7 +74,6 @@ class SweepPlan:
         plan_hz = np.array(sorted(self.get_hz()))
         good = True
         if len(hz)!=len(plan_hz):
-            print(f'{len(plan_hz)=} {len(hz)=}')
             good = False
         for h in hz:
             if not np.any(np.isclose(h,plan_hz)):
