scikit-rf/scikit-rf

commit c64db00b8d3ebf26b34c555693a5a8e77e5b4f0e

Fix write_touchstone with complex or non-equal z0 (#672)
* Fix write_touchstone with complex or non-equal z0

Use network port impedance as the reference impedance if possible.
Add 'r_ref' argument that can be used to specify reference impedance
to be used for renormalization.

* Remove is_from_hfss function

It was used to check if the file might have port impedances for
each frequency point. Instead check it when loading the file for the
first time.

* Rename hfss_port_impedances to has_hfss_port_impedances

skrf/network.py
@@ -1975,8 +1975,10 @@ class Network(object):
         self.port_names = touchstoneFile.port_names
 
         # set z0 before s so that y and z can be computed
-        if touchstoneFile.is_from_hfss():
+        if touchstoneFile.has_hfss_port_impedances:
             self.gamma, self.z0 = touchstoneFile.get_gamma_z0()
+            # This is the s_def that HFSS uses
+            self.s_def = 'traveling'
         else:
             self.z0 = complex(touchstoneFile.resistance)
         f, self.s = touchstoneFile.get_sparameter_arrays()  # note: freq in Hz
@@ -2051,7 +2053,7 @@ class Network(object):
                          write_z0: bool = False, skrf_comment: bool = True,
                          return_string: bool = False, to_archive: bool = None,
                          form: str = 'ri', format_spec_A: str = '{}', format_spec_B: str = '{}',
-                         format_spec_freq : str = '{}') -> Optional[str]:
+                         format_spec_freq : str = '{}', r_ref : float = None) -> Optional[str]:
         """
         Write a contents of the :class:`Network` to a touchstone file.
 
@@ -2085,7 +2087,11 @@ class Network(object):
         format_spec_freq : string, optional
             Any valid format specifying string as given by https://docs.python.org/3/library/string.html#format-string-syntax
             This specifies the formatting in the resulting touchstone file for the frequency.
-
+        r_ref : float
+            Reference impedance to renormalize the network.
+            If None network port impedance is used if possible. If None and
+            network port impedance is complex and not equal at all ports and
+            frequency points raises ValueError.
 
         Note
         ----
@@ -2102,14 +2108,36 @@ class Network(object):
         # according to Touchstone 2.0 spec
         # [no tab, max. 4 coeffs per line, etc.]
 
+        have_complex_ports = npy.any(self.z0.imag != 0)
+        equal_z0 = npy.all(self.z0 == self.z0[0, 0])
+
+        ntwk = self.copy()
+
+        if r_ref is None and write_z0 == False:
+            if not equal_z0:
+                raise ValueError("Network has unequal port impedances but reference impedance for renormalization 'r_ref' is not specified.")
+            if have_complex_ports:
+                raise ValueError("Network port impedances are complex but reference impedance for renormalization 'r_ref' is not specified.")
+            r_ref = ntwk.z0[0, 0]
+        elif r_ref is not None:
+            if not npy.isscalar(r_ref):
+                raise ValueError('r_ref must be scalar')
+            if r_ref.imag != 0:
+                raise ValueError('r_ref must be real')
+            ntwk.renormalize(r_ref)
+
+        if write_z0 and have_complex_ports and ntwk.s_def != 'traveling':
+            warnings.warn("Network s_def will be changed from {} to traveling to be compatible with HFSS. To silence this warning manually use Network.renormalize to change the s_def.".format(ntwk.s_def), UserWarning)
+            ntwk.renormalize(ntwk.z0, s_def='traveling')
+
         if filename is None:
-            if self.name is not None:
-                filename = self.name
+            if ntwk.name is not None:
+                filename = ntwk.name
             else:
                 raise ValueError('No filename given. Network must have a name, or you must provide a filename')
 
         if get_extn(filename) is None:
-            filename = filename + '.s%ip' % self.number_of_ports
+            filename = filename + '.s%ip' % ntwk.number_of_ports
 
         if dir is not None:
             filename = os.path.join(dir, filename)
@@ -2149,11 +2177,11 @@ class Network(object):
             return buf
 
         with get_buffer() as output:
-            # Add '!' Touchstone comment delimiters to the start of every line in self.comments
+            # Add '!' Touchstone comment delimiters to the start of every line in ntwk.comments
             commented_header = ''
             try:
-                if self.comments:
-                    for comment_line in self.comments.split('\n'):
+                if ntwk.comments:
+                    for comment_line in ntwk.comments.split('\n'):
                         commented_header += '!{}\n'.format(comment_line)
             except AttributeError:
                 pass
@@ -2166,36 +2194,40 @@ class Network(object):
             # the '#'  line is NOT a comment it is essential and it must be
             # exactly this format, to work
             # [HZ/KHZ/MHZ/GHZ] [S/Y/Z/G/H] [MA/DB/RI] [R n]
-            output.write('# {} S {} R {} \n'.format(self.frequency.unit, form, str(abs(self.z0[0, 0]))))
+            if write_z0:
+                output.write('!Data is not renormalized\n')
+                output.write('# {} S {} R\n'.format(ntwk.frequency.unit, form))
+            else:
+                output.write('# {} S {} R {} \n'.format(ntwk.frequency.unit, form, r_ref))
 
             # write ports
             try:
-                if self.port_names and len(self.port_names) == self.number_of_ports:
+                if ntwk.port_names and len(ntwk.port_names) == ntwk.number_of_ports:
                     ports = ''
-                    for port_idx, port_name in enumerate(self.port_names):
+                    for port_idx, port_name in enumerate(ntwk.port_names):
                         ports += '! Port[{}] = {}\n'.format(port_idx+1, port_name)
                     output.write(ports)
             except AttributeError:
                 pass
 
-            scaled_freq = self.frequency.f_scaled
+            scaled_freq = ntwk.frequency.f_scaled
 
-            if self.number_of_ports == 1:
+            if ntwk.number_of_ports == 1:
                 # write comment line for users (optional)
                 output.write('!freq {labelA}S11 {labelB}S11\n'.format(**formatDic))
                 # write out data
-                for f in range(len(self.f)):
+                for f in range(len(ntwk.f)):
                     output.write(format_spec_freq.format(scaled_freq[f]) + ' ' \
-                                 + c2str_A(self.s[f, 0, 0]) + ' ' \
-                                 + c2str_B(self.s[f, 0, 0]) + '\n')
+                                 + c2str_A(ntwk.s[f, 0, 0]) + ' ' \
+                                 + c2str_B(ntwk.s[f, 0, 0]) + '\n')
                     # write out the z0 following hfss's convention if desired
                     if write_z0:
                         output.write('! Port Impedance ')
-                        for n in range(self.number_of_ports):
-                            output.write('%.14f %.14f ' % (self.z0[f, n].real, self.z0[f, n].imag))
+                        for n in range(ntwk.number_of_ports):
+                            output.write('%.14f %.14f ' % (ntwk.z0[f, n].real, ntwk.z0[f, n].imag))
                         output.write('\n')
 
-            elif self.number_of_ports == 2:
+            elif ntwk.number_of_ports == 2:
                 # 2-port is a special case with
                 # - single line, and
                 # - S21,S12 in reverse order: legacy ?
@@ -2205,24 +2237,24 @@ class Network(object):
                     '!freq {labelA}S11 {labelB}S11 {labelA}S21 {labelB}S21 {labelA}S12 {labelB}S12 {labelA}S22 {labelB}S22\n'.format(
                         **formatDic))
                 # write out data
-                for f in range(len(self.f)):
+                for f in range(len(ntwk.f)):
                     output.write(format_spec_freq.format(scaled_freq[f]) + ' ' \
-                                 + c2str_A(self.s[f, 0, 0]) + ' ' \
-                                 + c2str_B(self.s[f, 0, 0]) + ' ' \
-                                 + c2str_A(self.s[f, 1, 0]) + ' ' \
-                                 + c2str_B(self.s[f, 1, 0]) + ' ' \
-                                 + c2str_A(self.s[f, 0, 1]) + ' ' \
-                                 + c2str_B(self.s[f, 0, 1]) + ' ' \
-                                 + c2str_A(self.s[f, 1, 1]) + ' ' \
-                                 + c2str_B(self.s[f, 1, 1]) + '\n')
+                                 + c2str_A(ntwk.s[f, 0, 0]) + ' ' \
+                                 + c2str_B(ntwk.s[f, 0, 0]) + ' ' \
+                                 + c2str_A(ntwk.s[f, 1, 0]) + ' ' \
+                                 + c2str_B(ntwk.s[f, 1, 0]) + ' ' \
+                                 + c2str_A(ntwk.s[f, 0, 1]) + ' ' \
+                                 + c2str_B(ntwk.s[f, 0, 1]) + ' ' \
+                                 + c2str_A(ntwk.s[f, 1, 1]) + ' ' \
+                                 + c2str_B(ntwk.s[f, 1, 1]) + '\n')
                     # write out the z0 following hfss's convention if desired
                     if write_z0:
                         output.write('! Port Impedance')
                         for n in range(2):
-                            output.write(' %.14f %.14f' % (self.z0[f, n].real, self.z0[f, n].imag))
+                            output.write(' %.14f %.14f' % (ntwk.z0[f, n].real, ntwk.z0[f, n].imag))
                         output.write('\n')
 
-            elif self.number_of_ports == 3:
+            elif ntwk.number_of_ports == 3:
                 # 3-port is written over 3 lines / matrix order
 
                 # write comment line for users (optional)
@@ -2233,21 +2265,21 @@ class Network(object):
                     output.write('\n!')
                 output.write('\n')
                 # write out data
-                for f in range(len(self.f)):
+                for f in range(len(ntwk.f)):
                     output.write(format_spec_freq.format(scaled_freq[f]))
                     for m in range(3):
                         for n in range(3):
-                            output.write(' ' + c2str_A(self.s[f, m, n]) + ' ' \
-                                         + c2str_B(self.s[f, m, n]))
+                            output.write(' ' + c2str_A(ntwk.s[f, m, n]) + ' ' \
+                                         + c2str_B(ntwk.s[f, m, n]))
                         output.write('\n')
                     # write out the z0 following hfss's convention if desired
                     if write_z0:
                         output.write('! Port Impedance')
                         for n in range(3):
-                            output.write(' %.14f %.14f' % (self.z0[f, n].real, self.z0[f, n].imag))
+                            output.write(' %.14f %.14f' % (ntwk.z0[f, n].real, ntwk.z0[f, n].imag))
                         output.write('\n')
 
-            elif self.number_of_ports >= 4:
+            elif ntwk.number_of_ports >= 4:
                 # general n-port
                 # - matrix is written line by line
                 # - 4 complex numbers / 8 real numbers max. for a single line
@@ -2257,29 +2289,29 @@ class Network(object):
 
                 # write comment line for users (optional)
                 output.write('!freq')
-                for m in range(1, 1 + self.number_of_ports):
-                    for n in range(1, 1 + self.number_of_ports):
+                for m in range(1, 1 + ntwk.number_of_ports):
+                    for n in range(1, 1 + ntwk.number_of_ports):
                         if (n > 0 and (n % 4) == 0):
                             output.write('\n!')
                         output.write(" {labelA}S{m}{n} {labelB}S{m}{n}".format(m=m, n=n, **formatDic))
                     output.write('\n!')
                 output.write('\n')
                 # write out data
-                for f in range(len(self.f)):
+                for f in range(len(ntwk.f)):
                     output.write(format_spec_freq.format(scaled_freq[f]))
-                    for m in range(self.number_of_ports):
-                        for n in range(self.number_of_ports):
+                    for m in range(ntwk.number_of_ports):
+                        for n in range(ntwk.number_of_ports):
                             if (n > 0 and (n % 4) == 0):
                                 output.write('\n')
-                            output.write(' ' + c2str_A(self.s[f, m, n]) + ' ' \
-                                         + c2str_B(self.s[f, m, n]))
+                            output.write(' ' + c2str_A(ntwk.s[f, m, n]) + ' ' \
+                                         + c2str_B(ntwk.s[f, m, n]))
                         output.write('\n')
 
                     # write out the z0 following hfss's convention if desired
                     if write_z0:
                         output.write('! Port Impedance')
-                        for n in range(self.number_of_ports):
-                            output.write(' %.14f %.14f' % (self.z0[f, n].real, self.z0[f, n].imag))
+                        for n in range(ntwk.number_of_ports):
+                            output.write(' %.14f %.14f' % (ntwk.z0[f, n].real, ntwk.z0[f, n].imag))
                         output.write('\n')
 
             if type(to_archive) is zipfile.ZipFile:

skrf/io/touchstone.py
@@ -117,20 +117,23 @@ class Touchstone:
         self.port_names = None
 
         self.comment_variables = None
-        
+
+        # Does the input file have HFSS per frequency port impedances
+        self.has_hfss_port_impedances = False
+
         # open the file depending on encoding
         # Guessing the encoding by trial-and-error, unless specified encoding
         try:
             if encoding is not None:
                 fid = get_fid(file, encoding=encoding)
                 self.filename = fid.name
-                self.load_file(fid)       
+                self.load_file(fid)
             else:
                 # Assume default encoding
                 fid = get_fid(file)
                 self.filename = fid.name
                 self.load_file(fid)
-            
+
         except UnicodeDecodeError:
             # Unicode fails -> Force Latin-1
             fid = get_fid(file, encoding='ISO-8859-1')
@@ -144,12 +147,12 @@ class Touchstone:
             self.load_file(fid)
 
         except Exception as e:
-            raise ValueError(f'Something went wrong by the file openning: {e}')
+            raise ValueError(f'Something went wrong by the file opening: {e}')
 
         self.gamma = []
         self.z0 = []
 
-        if self.is_from_hfss:
+        if self.has_hfss_port_impedances:
             self.get_gamma_z0_from_fid(fid)
 
         fid.close()
@@ -207,6 +210,8 @@ class Touchstone:
                             self.port_names[index - 1] = name
                     except ValueError as e:
                         print("Error extracting port names from line: {0}".format(line))
+                elif line[1].strip().lower().startswith('port impedance'):
+                    self.has_hfss_port_impedances = True
 
             # remove the comment (if any) so rest of line can be processed.
             # touchstone files are case-insensitive
@@ -504,24 +509,6 @@ class Touchstone:
         # noise_source_phase = noise_values[:,3]
         # noise_normalized_resistance = noise_values[:,4]
         raise NotImplementedError('not yet implemented')
-
-    def is_from_hfss(self):
-        """
-        Check if the Touchstone file has been produced by HFSS.
-
-        Returns
-        -------
-        status : boolean
-            True if the Touchstone file has been produced by HFSS
-            False otherwise
-        """
-        if self.comments is None:
-            return False
-
-        if 'exported from hfss' in str.lower(self.comments):
-            return True
-
-        return False
 
     def get_gamma_z0_from_fid(self, fid):
         """

skrf/tests/test_convenience.py
@@ -81,17 +81,18 @@ class ConvenienceTestCase(unittest.TestCase):
         
     def test_is_hfss_touchstone(self):
         """
-        Test if Touchstone files have been generated by HFSS or not
+        Test if Touchstone files have been generated by HFSS and has port
+        impedances for each frequency point.
         """
         # Touchstone file generated by HFSS      
-        self.assertTrue(rf.Touchstone(self.hfss_oneport_file).is_from_hfss() )
-        self.assertTrue(rf.Touchstone(self.hfss_twoport_file).is_from_hfss() )
-        self.assertTrue(rf.Touchstone(self.hfss_threeport_file).is_from_hfss() )
-        self.assertTrue(rf.Touchstone(self.hfss_18dot2).is_from_hfss())
-        self.assertTrue(rf.Touchstone(self.hfss_8ports).is_from_hfss())
-        self.assertTrue(rf.Touchstone(self.hfss_10ports).is_from_hfss())
+        self.assertTrue(rf.Touchstone(self.hfss_oneport_file).has_hfss_port_impedances)
+        self.assertTrue(rf.Touchstone(self.hfss_twoport_file).has_hfss_port_impedances)
+        self.assertTrue(rf.Touchstone(self.hfss_threeport_file).has_hfss_port_impedances)
+        self.assertTrue(rf.Touchstone(self.hfss_18dot2).has_hfss_port_impedances)
+        self.assertTrue(rf.Touchstone(self.hfss_8ports).has_hfss_port_impedances)
+        self.assertTrue(rf.Touchstone(self.hfss_10ports).has_hfss_port_impedances)
         # Touchstone file not from HFSS       
-        self.assertFalse(rf.Touchstone(os.path.join(self.test_dir, 'ntwk1.s2p')).is_from_hfss() )
+        self.assertFalse(rf.Touchstone(os.path.join(self.test_dir, 'ntwk1.s2p')).has_hfss_port_impedances)
         
     def test_hfss_touchstone_2_network(self):
         """

skrf/tests/test_network.py
@@ -264,6 +264,68 @@ class NetworkTestCase(unittest.TestCase):
         self.assertEqual(self.ntwk1, ntwk1Saved)
         os.remove(os.path.join(self.test_dir, 'ntwk1Saved.s2p'))
 
+    def test_write_touchstone(self):
+        ports = 2
+        s_random = npy.random.uniform(-1, 1, (self.freq.npoints, ports, ports)) +\
+                1j * npy.random.uniform(-1, 1, (self.freq.npoints, ports, ports))
+        random_z0 = npy.random.uniform(1, 100, (self.freq.npoints, ports)) +\
+                    1j * npy.random.uniform(-100, 100, (self.freq.npoints, ports))
+        ntwk = rf.Network(s=s_random, frequency=self.freq, z0=random_z0, name='test_ntwk', s_def='traveling')
+
+        # Writing a network with non-constant z0 should raise
+        with pytest.raises(ValueError) as e_info:
+            snp = ntwk.write_touchstone(return_string=True)
+
+        ntwk.s_def = 'power'
+        # Should raise warning when write_z0 and s_def != 'traveling'.
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            snp = ntwk.write_touchstone(return_string=True, write_z0=True)
+            self.assertTrue(len(w) == 1)
+
+        ntwk.s_def = 'traveling'
+        # Ok if write_z0 specified
+        snp = ntwk.write_touchstone(return_string=True, write_z0=True)
+
+        # Test reading it back
+        strio = io.StringIO(snp)
+        # Required for reading touchstone file
+        strio.name = 'StringIO.s{}p'.format(ports)
+        ntwk2 = rf.Network(strio)
+
+        npy.testing.assert_allclose(ntwk2.s, s_random)
+        npy.testing.assert_allclose(ntwk2.z0, random_z0)
+
+        # Renormalize output to 50 ohms
+        snp = ntwk.write_touchstone(return_string=True, r_ref=50)
+
+        # Network should not have been modified
+        self.assertTrue(npy.all(ntwk.s == s_random))
+        self.assertTrue(npy.all(ntwk.z0 == random_z0))
+
+        # Read back the written touchstone
+        strio = io.StringIO(snp)
+        strio.name = 'StringIO.s{}p'.format(ports)
+        ntwk2 = rf.Network(strio)
+
+        # Renormalize original network to match the written one
+        ntwk.renormalize(50)
+
+        npy.testing.assert_allclose(ntwk.s, ntwk2.s)
+        npy.testing.assert_allclose(ntwk.z0, ntwk2.z0)
+
+        ntwk.z0[0, 0] = 1
+        # Writing network with non-constant real z0 should fail without r_ref
+        with pytest.raises(ValueError) as e_info:
+            snp = ntwk.write_touchstone(return_string=True)
+        ntwk.z0[0, 0] = 50
+
+        ntwk.renormalize(50 + 1j)
+
+        # Writing complex characteristic impedance should fail
+        with pytest.raises(ValueError) as e_info:
+            snp = ntwk.write_touchstone(return_string=True)
+
     def test_pickling(self):
         original_ntwk = self.ntwk1
         with tempfile.NamedTemporaryFile(dir=self.test_dir, suffix='ntwk') as fid:

commit f023554590719377ee87ba827cf1cef907c44d26

write_touchstone: don't write z_ref in complex format if it's real, fix #724.
In scikit-rf v0.23.0, a regression was introduced in write_touchstone(),
the port reference impedance is given as a complex number even when it's
real.

    !Created with skrf (http://scikit-rf.org).
    # MHz S RI R (50+0j)

This is a regression introduced by #672 after complex impedance support
(#652) was added. Before this change, the reference impedance was always
given as a real number. Since complex impedance is not officially
supported by Touchstone v1.1 or v2. It means this change causes scikit-rf
to output an invalid file and it's breaking compatibility with nearly all
applications.

In fact, a complex impedance should never appear here. In the beginning
of write_touchstone(), we already checked and ensured that the imaginary
part of the impedance must be zero when writing a standard (non-HFSS)
Touchstone file.

This commit fixes the problem by writing "r_ref.real" instead of "r_ref",
so we get a real number "a" instead of a complex number "(a+0j)". An
assertion statement was also added to ensure the imaginary part of the
complex impedance must be zero - the complex impedance scenario should
never happen.

Signed-off-by: Yifeng Li <tomli@tomli.me>

skrf/network.py
@@ -2282,7 +2282,14 @@ class Network(object):
                 output.write('!Data is not renormalized\n')
                 output.write('# {} S {} R\n'.format(ntwk.frequency.unit, form))
             else:
-                output.write('# {} S {} R {} \n'.format(ntwk.frequency.unit, form, r_ref))
+                # Write "r_ref.real" instead of "r_ref", so we get a real number "a" instead
+                # of a complex number "(a+0j)", which is unsupported by the standard Touchstone
+                # format (non-HFSS). We already checked in the beginning that "r_ref" must be
+                # real in this case (write_z0 == False).
+                assert r_ref.imag == 0, "Complex reference impedance is encountered when " \
+                                        "generating a standard Touchstone (non-HFSS), this " \
+                                        "should never happen in scikit-rf."
+                output.write('# {} S {} R {} \n'.format(ntwk.frequency.unit, form, r_ref.real))
 
             # write ports
             try: