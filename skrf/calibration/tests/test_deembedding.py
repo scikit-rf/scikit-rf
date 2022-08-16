bf92c65e6d37d1eb3e82108a527349c591ac71f8
Keisuke Kawahara <k.kawahara0@gmail.com>
2022-08-16 12:21:38 +0900

    Add independent short and open tests

--- skrf/calibration/tests/test_deembedding.py
+++ skrf/calibration/tests/test_deembedding.py
@@ -9,7 +9,7 @@ class DeembeddingTestCase(unittest.TestCase):
     """
     Testcase for the Deembedding class
     Pseudo-netlists for s-parameter files used in these tests
-    For open-short, open and short de-embedding:
+    For open-short de-embedding:
     - deemb_ind.s2p
         P1      (1 0) port
         Cpad1   (1 0) capacitor c=25fF
@@ -35,6 +35,20 @@ class DeembeddingTestCase(unittest.TestCase):
         Cpad2   (4 0) capacitor c=25fF
         Cp2p    (1 4) capacitor c=10fF
         P2      (4 0) port
+    For open de-embedding:
+    - deemb_ind7.s2p
+        P1      (1 0) port
+        Cpad1   (1 0) capacitor c=25fF
+        Dut_ind (1 2) inductor l=1nH
+        Cpad2   (2 0) capacitor c=25fF
+        Cp2p    (1 2) capacitor c=10fF
+        P2      (2 0) port
+    - deemb_open7.s2p
+        P1      (1 0) port
+        Cpad1   (1 0) capacitor c=25fF
+        Cpad2   (2 0) capacitor c=25fF
+        Cp2p    (1 2) capacitor c=10fF
+        P2      (2 0) port
     For short-open de-embedding:
     - deemb_ind2.s2p
         P1      (1 0) port
@@ -59,6 +73,18 @@ class DeembeddingTestCase(unittest.TestCase):
         Cpad1   (0 0) capacitor c=25fF
         Cpad2   (0 0) capacitor c=25fF
         Cp2p    (0 0) capacitor c=10fF
+        Rline2  (0 4) resistor r=2ohm
+        P2      (4 0) port
+    For short de-embedding:
+    - deemb_ind8.s2p
+        P1      (1 0) port
+        Rline1  (1 2) resistor r=2ohm
+        Dut_ind (2 3) inductor l=1nH
+        Rline2  (3 4) resistor r=2ohm
+        P2      (4 0) port
+    - deemb_short8.s2p
+        P1      (1 0) port
+        Rline1  (1 0) resistor r=2ohm
         Rline2  (0 4) resistor r=2ohm
         P2      (4 0) port
     For SplitPi de-embedding:
@@ -125,7 +151,7 @@ class DeembeddingTestCase(unittest.TestCase):
         """
         self.test_dir = os.path.dirname(os.path.abspath(__file__))+'/deembed/'
         
-        # for open-short, open and short testing
+        # for open-short testing
         self.raw = rf.Network(os.path.join(self.test_dir, 'deemb_ind.s2p'))
         self.open = rf.Network(os.path.join(self.test_dir, 'deemb_open.s2p'))
         self.short = rf.Network(os.path.join(self.test_dir, 'deemb_short.s2p'))
@@ -135,7 +161,15 @@ class DeembeddingTestCase(unittest.TestCase):
         self.open_1f = self.open['10GHz'] 
         self.short_1f = self.short['10GHz']
 
-        # for short-open testing
+        # for open testing
+        self.raw7 = rf.Network(os.path.join(self.test_dir, 'deemb_ind7.s2p'))
+        self.open7 = rf.Network(os.path.join(self.test_dir, 'deemb_open7.s2p'))
+
+        # for spot frequency checking
+        self.raw7_1f = self.raw7['10GHz'] 
+        self.open7_1f = self.open7['10GHz'] 
+
+        # for short-open and short testing
         self.raw2 = rf.Network(os.path.join(self.test_dir, 'deemb_ind2.s2p'))
         self.open2 = rf.Network(os.path.join(self.test_dir, 'deemb_open2.s2p'))
         self.short2 = rf.Network(os.path.join(self.test_dir, 'deemb_short2.s2p'))
@@ -144,6 +178,14 @@ class DeembeddingTestCase(unittest.TestCase):
         self.raw2_1f = self.raw2['10GHz'] 
         self.open2_1f = self.open2['10GHz'] 
         self.short2_1f = self.short2['10GHz']
+
+        # for short testing
+        self.raw8 = rf.Network(os.path.join(self.test_dir, 'deemb_ind8.s2p'))
+        self.short8 = rf.Network(os.path.join(self.test_dir, 'deemb_short8.s2p'))
+        
+        # for spot frequency checking
+        self.raw8_1f = self.raw8['10GHz'] 
+        self.short8_1f = self.short8['10GHz']
 
         # for spilit-pi testing
         self.raw3 = rf.Network(os.path.join(self.test_dir, 'deemb_ind3.s2p'))
@@ -180,9 +222,9 @@ class DeembeddingTestCase(unittest.TestCase):
         # create de-embedding objects
         self.dm = rf.OpenShort(self.open, self.short)
         self.dm_os = rf.OpenShort(self.open_1f, self.short_1f) 
-        self.dm_o = rf.Open(self.open_1f)
-        self.dm_s = rf.Short(self.short_1f)
+        self.dm_o = rf.Open(self.open7_1f)
         self.dm_so = rf.ShortOpen(self.short2_1f, self.open2_1f)
+        self.dm_s = rf.Short(self.short8_1f)
         self.dm_pi = rf.SplitPi(self.thru3_1f)
         self.dm_tee = rf.SplitTee(self.thru4_1f)
         self.dm_ac = rf.AdmittanceCancel(self.thru5_1f)
@@ -212,32 +254,28 @@ class DeembeddingTestCase(unittest.TestCase):
 
     def test_open(self):
         """
-        After open de-embedding, the network is a R-L-R network with 2ohm-1nH-2ohm.
+        After de-embedding, the network is a pure inductor of 1nH.
         Test that this is true at a spot frequency.
         """
-        dut = self.dm_o.deembed(self.raw_1f)
-        res_calc = np.real(1/dut.y[0,0,0])
-        ind_calc = 1e9*np.imag(1/dut.y[0,0,0])/2/np.pi/dut.f
-        self.assertTrue(np.isclose(res_calc, 4, rtol=self.rtol))
-        self.assertTrue(np.isclose(ind_calc, 1, rtol=self.rtol))
-
-    def test_short(self):
-        """
-        First do open de-embedding, and next short. It should give pure inductor of 1nH.
-        This is similar to OpenShort, but done in 2 steps.
-        """
-        raw_minus_open = self.dm_o.deembed(self.raw_1f)
-        dut = self.dm_s.deembed(raw_minus_open)
+        dut = self.dm_o.deembed(self.raw7_1f)
         ind_calc = 1e9*np.imag(1/dut.y[0,0,0])/2/np.pi/dut.f
         self.assertTrue(np.isclose(ind_calc, 1, rtol=self.rtol))
 
     def test_shortopen(self):
         """
-        First do short de-embedding to remove 2ohm series resistors on each side, then remove
-        open shunt capacitors which are 25fF to ground on each pad, and 10fF pad-to-pad.
-        The resulting network should be a pure inductor of 1nH.
+        After de-embedding, the network is a pure inductor of 1nH.
+        Test that this is true at a spot frequency.
         """
         dut = self.dm_so.deembed(self.raw2_1f)
+        ind_calc = 1e9*np.imag(1/dut.y[0,0,0])/2/np.pi/dut.f
+        self.assertTrue(np.isclose(ind_calc, 1, rtol=self.rtol))
+
+    def test_short(self):
+        """
+        After de-embedding, the network is a pure inductor of 1nH.
+        Test that this is true at a spot frequency.
+        """
+        dut = self.dm_s.deembed(self.raw8_1f)
         ind_calc = 1e9*np.imag(1/dut.y[0,0,0])/2/np.pi/dut.f
         self.assertTrue(np.isclose(ind_calc, 1, rtol=self.rtol))
 
