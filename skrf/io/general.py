a0666eb92e258de9d8cc84fb22748cfd71706bb5
xafra <68167524+xafra@users.noreply.github.com>
2022-09-03 09:08:25 +0200

    Update general.py

--- skrf/io/general.py
+++ skrf/io/general.py
@@ -249,7 +249,7 @@ def write(file, obj, overwrite = True):
         pickle.dump(obj, fid, protocol=2)
         fid.close()
 
-def read_all(dir: str ='.', sort=True, contains = None, f_unit = None, obj_type=None, files: list=None, recursive=False) -> dict:
+def read_all(dir: str ='.', sort = True, contains = None, f_unit = None, obj_type=None, files: list=None, recursive=False) -> dict:
     """
     Read all skrf objects in a directory.
 
