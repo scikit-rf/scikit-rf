c5291547ee57daee2266c8be8cd33813370b2659
Bailey Campbell <rdcampbell1990@gmail.com>
2023-04-05 11:31:14 -0400

    Add N5227B support

--- skrf/vi/vna/keysight/pna.py
+++ skrf/vi/vna/keysight/pna.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, Sequence
+    from typing import Optional, Sequence, Union
 
 import re
 import sys
 from enum import Enum
+import itertools
 
 import numpy as np
 
@@ -32,23 +33,27 @@ class SweepType(Enum):
     SEGMENT = "SEGM"
     PHASE = "PHAS"
 
+
 class SweepMode(Enum):
     HOLD = "HOLD"
     CONTINUOUS = "CONT"
     GROUPS = "GRO"
     SINGLE = "SING"
 
+
 class TriggerSource(Enum):
     EXTERNAL = "EXT"
     IMMEDIATE = "IMM"
     MANUAL = "MAN"
 
+
 class AveragingMode(Enum):
     POINT = "POIN"
     SWEEP = "SWE"
 
+
 class PNA(VNA):
-    # TODO: 
+    # TODO:
     # - active_calset_name: SENS<self:cnum>:CORR:CSET:ACT? NAME
     # - create_calset: SENS<self:cnum>:CORR:CSET:CRE <name>
     # - calset_data: SENS<self:cnum>:CORR:CSET:DATA?  <eterm>,<port a>,<port b> '<receiver>'
@@ -67,14 +72,9 @@ class PNA(VNA):
     # N5241A, N5242A, N5244A, N5245A, N5247A, N5249A, N5247B, N5245B, N5244B
     # N5242B, N5241B, N5249B, N5264A, N5264B
     _models = {
-        'default': {
-            'nports': 2,
-            'unsupported': []
-        },
-        'E8362C': {
-            'nports': 2,
-            'unsupported': ['nports', 'freq_step', 'fast_sweep']
-        },
+        "default": {"nports": 2, "unsupported": []},
+        "E8362C": {"nports": 2, "unsupported": ["nports", "freq_step", "fast_sweep"]},
+        "N5227B": {"nports": 4, "unsupported": []},
     }
 
     class Channel(vna.Channel):
@@ -83,7 +83,7 @@ class PNA(VNA):
 
             if cnum != 1:
                 default_msmnt = f"CH{self.cnum}_S11_1"
-                self.create_measurement(default_msmnt, 'S11')
+                self.create_measurement(default_msmnt, "S11")
 
         def _on_delete(self):
             self.write(f"SYST:CHAN:DEL {self.cnum}")
@@ -156,36 +156,36 @@ class PNA(VNA):
         measurement_numbers = VNA.command(
             get_cmd="SYST:MEAS:CAT? <self:cnum>",
             set_cmd=None,
-            doc="""The list of measurement numbers on this channel""", 
+            doc="""The list of measurement numbers on this channel""",
             validator=DelimitedStrValidator(int),
         )
 
         averaging_on = VNA.command(
             get_cmd="SENS<self:cnum>:AVER:STATE?",
             set_cmd="SENS<self:cnum>:AVER:STATE <arg>",
-            doc="""Whether averaging is on or off""", 
+            doc="""Whether averaging is on or off""",
             validator=BooleanValidator(),
         )
 
         averaging_count = VNA.command(
             get_cmd="SENS<self:cnum>:AVER:COUN?",
             set_cmd="SENS<self:cnum>:AVER:COUN <arg>",
-            doc="""The number of measurements combined for an average""", 
+            doc="""The number of measurements combined for an average""",
             validator=IntValidator(1, 65536),
         )
 
         averaging_mode = VNA.command(
             get_cmd="SENS<self:cnum>:AVER:MODE?",
             set_cmd="SENS<self:cnum>:AVER:MODE <arg>",
-            doc="""How measurements are averaged together""", 
+            doc="""How measurements are averaged together""",
             validator=EnumValidator(AveragingMode),
         )
 
         n_sweep_groups = VNA.command(
             get_cmd="SENS<self:cnum>:SWE:GRO:COUN?",
             set_cmd="SENS<self:cnum>:SWE:GRO:COUN <arg>",
-            doc="""The number of triggers sent for one trigger command""", 
-            validator=IntValidator(1, int(2E6)),
+            doc="""The number of triggers sent for one trigger command""",
+            validator=IntValidator(1, int(2e6)),
         )
 
         active_trace_sdata = VNA.command(
@@ -193,7 +193,7 @@ class PNA(VNA):
             set_cmd=None,
             doc="""Get the current trace's s-values as a numpy array""",
             values=True,
-            complex_values=True
+            complex_values=True,
         )
 
         @property
@@ -203,17 +203,19 @@ class PNA(VNA):
             return int(f.step)
 
         @freq_step.setter
-        def freq_step(self, f: int) -> None:
-            freq = self.frequeny
+        def freq_step(self, f: Union[int, float, str]) -> None:
+            validator = FreqValidator()
+            f = validator.validate_input(f)
+            freq = self.frequency
             self.npoints = len(range(int(freq.start), int(freq.stop) + f, f))
 
         @property
         def frequency(self) -> skrf.Frequency:
             f = skrf.Frequency(
-                start=self.freq_start, 
+                start=self.freq_start,
                 stop=self.freq_stop,
                 npoints=self.npoints,
-                unit='hz'
+                unit="hz",
             )
             return f
 
@@ -225,8 +227,8 @@ class PNA(VNA):
 
         @property
         def measurements(self) -> list[tuple[str, str]]:
-            msmnts = self.query(f"CALC{self.cnum}:PAR:CAT:EXT?").replace('"', '')
-            msmnts = msmnts.split(',')
+            msmnts = self.query(f"CALC{self.cnum}:PAR:CAT:EXT?").replace('"', "")
+            msmnts = msmnts.split(",")
             return list(zip(msmnts[::2], msmnts[1::2]))
 
         @property
@@ -255,15 +257,13 @@ class PNA(VNA):
             for term in terms:
                 pna_name = term[0]
                 skrf_name = re.sub(
-                    r"([A-Z])",
-                    lambda pat: f" {pat.group(1).lower()}",
-                    pna_name
+                    r"([A-Z])", lambda pat: f" {pat.group(1).lower()}", pna_name
                 ).strip()
 
                 coeffs[skrf_name] = self.query_values(
                     f"SENS{self.cnum}:CORR:CSET:ETERM? '{pna_name}({a},{b})'",
                     complex_values=True,
-                    container=np.array
+                    container=np.array,
                 )
 
             self.parent.query_format = orig_query_fmt
@@ -281,18 +281,17 @@ class PNA(VNA):
         def create_measurement(self, name: str, parameter: str) -> None:
             self.write(f"CALC{self.cnum}:PAR:EXT '{name}',{parameter}")
             # Not all instruments support DISP:WIND:TRAC:NEXT
-            traces = self.query("DISP:WIND:CAT?").replace('"', '')
-            traces = [int(tr) for tr in traces.split(',')]
+            traces = self.query("DISP:WIND:CAT?").replace('"', "")
+            traces = [int(tr) for tr in traces.split(",")]
             next_tr = traces[-1] + 1
             self.write(f"DISP:WIND:TRAC{next_tr}:FEED '{name}'")
 
         def delete_measurement(self, name: str) -> None:
             self.write(f"CALC{self.cnum}:PAR:DEL '{name}'")
-        
+
         def get_measurement(self, name: str) -> skrf.Network:
             if name not in self.measurement_names:
                 raise KeyError(f"{name} does not exist")
-                return
 
             self.parent.active_measurement = name
             ntwk = self.get_active_trace()
@@ -306,28 +305,41 @@ class PNA(VNA):
 
             ntwk = skrf.Network()
             ntwk.frequency = self.frequency
-            ntwk.s = np.empty(
-                shape=(ntwk.frequency.npoints, 1, 1), dtype=complex
-            )
-            raw = self.active_trace_sdata
-            ntwk.s[:, 0, 0] = raw[::2] + 1j * raw[1::2]
+            ntwk.s = self.active_trace_sdata
 
             self.parent.query_format = orig_query_fmt
 
             return ntwk
 
+        def get_sdata(self, a: int | str, b: int | str) -> skrf.Network:
+            self.sweep()
+            orig_query_fmt = self.parent.query_format
+            self.parent.query_format = ValuesFormat.BINARY_64
+            param = f"S{a}{b}"
+            self.create_measurement("SKRF_TMP", param)
+            self.parent.active_measurement = "SKRF_TMP"
+
+            ntwk = skrf.Network()
+            ntwk.frequency = self.frequency
+            ntwk.s = self.active_trace_sdata
+
+            self.delete_measurement("SKRF_TMP")
+            self.parent.query_format = orig_query_fmt
+
+            return ntwk
+
         def get_snp_network(
-            self, 
-            ports: Sequence={1,2}, 
+            self,
+            ports: Optional[Sequence] = None,
         ) -> skrf.Network:
-            if not ports:
-                ports = list(range(1, self.parent.nports+1))
-            
+            if ports is None:
+                ports = list(range(1, self.parent.nports + 1))
+
             orig_query_fmt = self.parent.query_format
             self.parent.query_format = ValuesFormat.BINARY_64
             self.parent.active_channel = self
 
-            msmnt_params = [f"S{p}{p}" for p in ports]
+            msmnt_params = [f"S{a}{b}" for a, b in itertools.product(ports, repeat=2)]
 
             names = []
             # Make sure the ports specified are driven
@@ -338,8 +350,10 @@ class PNA(VNA):
                 self.create_measurement(name, param)
 
             self.sweep()
-            port_str = ','.join(str(port) for port in ports)
-            raw = self.query_values(f"CALC{self.cnum}:DATA:SNP:PORTS? '{port_str}'", container=np.array)
+            port_str = ",".join(str(port) for port in ports)
+            raw = self.query_values(
+                f"CALC{self.cnum}:DATA:SNP:PORTS? '{port_str}'", container=np.array
+            )
             self.parent.wait_for_complete()
 
             for name in names:
@@ -353,7 +367,7 @@ class PNA(VNA):
             #   [s12.real],
             #   [s12.imag],
             # ...
-            # ] 
+            # ]
             # but flattened. So we recreate the above shape from the flattened data
             npoints = self.npoints
             nrows = len(raw) // npoints
@@ -386,11 +400,11 @@ class PNA(VNA):
             avg_mode = self.averaging_mode
 
             original_config = {
-                'sweep_mode': sweep_mode,
-                'sweep_time': sweep_time,
-                'averaging_on': avg_on,
-                'averaging_mode': avg_mode,
-                'timeout': self.parent._resource.timeout
+                "sweep_mode": sweep_mode,
+                "sweep_time": sweep_time,
+                "averaging_on": avg_on,
+                "averaging_mode": avg_mode,
+                "timeout": self.parent._resource.timeout,
             }
 
             if avg_on and avg_mode == AveragingMode.SWEEP:
@@ -403,47 +417,48 @@ class PNA(VNA):
                 n_sweeps = self.parent.nports
 
             try:
-                sweep_time *= (n_sweeps * 1_000) # 1s per port
-                self.parent._resource.timeout = max(sweep_time, 5_000) # minimum of 5s
+                sweep_time *= n_sweeps * 1_000  # 1s per port
+                self.parent._resource.timeout = max(sweep_time, 5_000)  # minimum of 5s
                 self.parent.wait_for_complete()
             finally:
                 self.parent._resource.clear()
-                self.parent._resource.timeout = original_config.pop('timeout')
-                for k,v in original_config.items():
+                self.parent._resource.timeout = original_config.pop("timeout")
+                for k, v in original_config.items():
                     setattr(self, k, v)
 
-    def __init__(self, address: str, backend: str = '@py') -> None:
+    def __init__(self, address: str, backend: str = "@py") -> None:
         super().__init__(address, backend)
 
-        self._resource.read_termination = '\n'
-        self._resource.write_termination = '\n'
+        self._resource.read_termination = "\n"
+        self._resource.write_termination = "\n"
 
         self.create_channel(1, "Channel 1")
         self.active_channel = self.ch1
 
-        self.model = self.id.split(',')[1]
+        self.model = self.id.split(",")[1]
         if self.model not in self._models:
             print(
                 f"WARNING: This model ({self.model}) has not been tested with "
                 "scikit-rf. By default, all features are turned on but older "
                 "instruments might be missing SCPI support for some commands "
                 "which will cause errors. Consider submitting an issue on GitHub to "
-                "help testing and adding support.", file=sys.stderr
+                "help testing and adding support.",
+                file=sys.stderr,
             )
 
     def _supports(self, feature: str) -> bool:
-        model_config = self._models.get(self.model, self._models['default'])
-        return feature not in model_config['unsupported']
+        model_config = self._models.get(self.model, self._models["default"])
+        return feature not in model_config["unsupported"]
 
     def _model_param(self, param: str):
-        model_config = self._models.get(self.model, self._models['default'])
+        model_config = self._models.get(self.model, self._models["default"])
         return model_config[param]
-    
+
     trigger_source = VNA.command(
-        get_cmd='TRIG:SOUR?',
-        set_cmd='TRIG:SOUR <arg>',
+        get_cmd="TRIG:SOUR?",
+        set_cmd="TRIG:SOUR <arg>",
         doc="""The source of the sweep trigger signal""",
-        validator=EnumValidator(TriggerSource)
+        validator=EnumValidator(TriggerSource),
     )
 
     nerrors = VNA.command(
@@ -451,26 +466,26 @@ class PNA(VNA):
         set_cmd=None,
         doc="""The number of errors since last cleared (see
             :func:`PNA.clear_errors`)""",
-        validator=IntValidator()
+        validator=IntValidator(),
     )
 
     channel_numbers = VNA.command(
         get_cmd="SYST:CHAN:CAT?",
-        set_cmd = None,
+        set_cmd=None,
         doc="""The channel numbers currently in use""",
-        validator=DelimitedStrValidator(int)
+        validator=DelimitedStrValidator(int),
     )
 
     @property
     def nports(self) -> int:
-        if self._supports('nports'):
+        if self._supports("nports"):
             return int(self.query("SYST:CAP:HARD:PORT:COUN?"))
         else:
-            return self._model_param('nports')
+            return self._model_param("nports")
 
     @property
     def active_channel(self) -> Optional[Channel]:
-        num = int(self.query('SYST:ACT:CHAN?'))
+        num = int(self.query("SYST:ACT:CHAN?"))
         return getattr(self, f"ch{num}", None)
 
     @active_channel.setter
@@ -479,11 +494,11 @@ class PNA(VNA):
             return
 
         msmnt = ch.measurement_numbers[0]
-        self.write(f'CALC{ch.cnum}:PAR:MNUM {msmnt}')
+        self.write(f"CALC{ch.cnum}:PAR:MNUM {msmnt}")
 
     @property
     def query_format(self) -> vna.ValuesFormat:
-        fmt = self.query("FORM?").replace('+', '')
+        fmt = self.query("FORM?").replace("+", "")
         if fmt == "ASC,0":
             self._values_fmt = vna.ValuesFormat.ASCII
         elif fmt == "REAL,32":
@@ -508,20 +523,20 @@ class PNA(VNA):
 
     @property
     def active_measurement(self) -> str:
-        return self.query("SYST:ACT:MEAS?").replace('"', '')
+        return self.query("SYST:ACT:MEAS?").replace('"', "")
 
     @active_measurement.setter
     def active_measurement(self, name: str) -> None:
         measurements = {
             name: channel
-            for channel in self.channels 
+            for channel in self.channels
             for name in channel.measurement_names
         }
 
         if name not in measurements:
             raise KeyError(f"{name} does not exist")
 
-        if self._supports('fast_sweep'):
+        if self._supports("fast_sweep"):
             self.write(f"CALC{measurements[name].cnum}:PAR:SEL '{name}',fast")
         else:
             self.write(f"CALC{measurements[name].cnum}:PAR:SEL '{name}'")
