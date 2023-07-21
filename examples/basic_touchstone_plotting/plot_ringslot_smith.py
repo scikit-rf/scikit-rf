04929fe5e3566fd3da4e87c3cbc23441c8d0fd00
Julien Hillairet <julien.hillairet@gmail.com>
2023-07-21 17:24:51 +0200

    removing examples directory

--- examples/basic_touchstone_plotting/plot_ringslot_smith.py
+++
@@ -1,9 +0,0 @@
-import pylab
-import skrf as rf
-
-ring_slot= rf.Network('ring slot array measured.s1p')
-
-pylab.figure(1)
-pylab.title('WR-10 Ringslot Array, Smith')
-ring_slot.plot_s_smith(m=0,n=0) # m,n are S-Matrix indices
-pylab.show()
