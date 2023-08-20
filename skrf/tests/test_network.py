3caed6b9635a70f841e94047604c9c56258179f3
Keisuke Kawahara <k.kawahara0@gmail.com>
2023-08-21 03:43:36 +0900

    Fix test

--- skrf/tests/test_network.py
+++ skrf/tests/test_network.py
@@ -1786,12 +1786,12 @@ class NetworkTestCase(unittest.TestCase):
         load_stability_circle_ads = npy.loadtxt(os.path.join(self.test_dir, 'load_stability_circle_ads.csv'), encoding='utf-8', delimiter=',')
         self.assertTrue(
             npy.all(
-                npy.abs(rf.complex_2_magnitude(self.fet['30GHz'].load_stability_circle(npoints=51)) - load_stability_circle_ads[:,0]) < 1e-6
+                npy.abs(rf.complex_2_magnitude(self.fet['30GHz'].load_stability_circle(npoints=6)[:,0]) - load_stability_circle_ads[:,0]) / load_stability_circle_ads[:,0] < 1e-4
             )
         )
         self.assertTrue(
             npy.all(
-                npy.abs(rf.complex_2_degree(self.fet['30GHz'].load_stability_circle(npoints=51)) - load_stability_circle_ads[:,1]) < 1e-6
+                npy.abs(rf.complex_2_degree(self.fet['30GHz'].load_stability_circle(npoints=6)[:,0]) - load_stability_circle_ads[:,1]) / load_stability_circle_ads[:,1] < 1e-4
             )
         )
 
@@ -1809,12 +1809,12 @@ class NetworkTestCase(unittest.TestCase):
         source_stability_circle_ads = npy.loadtxt(os.path.join(self.test_dir, 'source_stability_circle_ads.csv'), encoding='utf-8', delimiter=',')
         self.assertTrue(
             npy.all(
-                npy.abs(rf.complex_2_magnitude(self.fet['30GHz'].source_stability_circle(npoints=51)) - source_stability_circle_ads[:,0]) < 1e-6
+                npy.abs(rf.complex_2_magnitude(self.fet['30GHz'].source_stability_circle(npoints=6)[:,0]) - source_stability_circle_ads[:,0]) / source_stability_circle_ads[:,0] < 1e-4
             )
         )
         self.assertTrue(
             npy.all(
-                npy.abs(rf.complex_2_degree(self.fet['30GHz'].source_stability_circle(npoints=51)) - source_stability_circle_ads[:,1]) < 1e-6
+                npy.abs(rf.complex_2_degree(self.fet['30GHz'].source_stability_circle(npoints=6)[:,0]) - source_stability_circle_ads[:,1]) / source_stability_circle_ads[:,1] < 1e-4
             )
         )
 
