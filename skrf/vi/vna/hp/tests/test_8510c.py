fb1e192040c462ad050398f2b212c278a71ec888
Jonathan deWerd <jon@jdewerd.com>
2024-03-09 11:37:22 -0500

    Updated 8510 class with bugfixes and compound sweep support

--- skrf/vi/vna/hp/tests/test_8510c.py
+++ skrf/vi/vna/hp/tests/test_8510c.py
@@ -30,7 +30,6 @@ def mocked_ff(mocker):
         ('id', 'OUTPIDEN;', None, 'HP8510C.07.14: Aug 26  1998', 'HP8510C.07.14: Aug 26  1998', None),
         ('freq_start', 'STAR;OUTPACTI;', 'STEP; STAR 100;', '100', 100, 100),
         ('freq_stop', 'STOP;OUTPACTI;', 'STEP; STOP 100;', '100', 100, 100),
-        ('npoints', 'POIN;OUTPACTI;', 'STEP; POIN 101;', '101', 101, 101),
         ('is_continuous', 'GROU?', 'CONT;', '"CONTINUAL"', True, True),
     ]
 )
@@ -44,6 +43,7 @@ def test_params(
     expected_val,
     write_val
 ):
+    mocked_ff._resource = None
     if expected_query is not None:
         mocked_ff.query.return_value = query_response
         test_val = getattr(mocked_ff, param)
@@ -54,10 +54,6 @@ def test_params(
         setattr(mocked_ff, param, write_val)
         mocked_ff.write.assert_called_once_with(expected_write)
 
-def test_npoint_raises(mocker, mocked_ff):
-    with pytest.raises(ValidationError):
-        mocked_ff.npoints = 100
-
 def test_freq_query(mocker, mocked_ff):
     mocked_ff.query.side_effect = [
         '100', '200', '51'
@@ -67,27 +63,15 @@ def test_freq_query(mocker, mocked_ff):
 
 def test_freq_write(mocker, mocked_ff):
     mocked_ff._resource = mocker.MagicMock()
+    mocked_ff.min_hz = 100
+    mocked_ff.max_hz = 100000
     test_f = skrf.Frequency(100, 200, 51, unit='hz')
     mocked_ff.frequency = test_f
-    mocked_ff.write.assert_called_once_with("STEP; STAR 100; STOP 200; POIN51;")
-
-def test_query_fmt_query(mocker, mocked_ff):
-    mocked_ff._values_fmt = ValuesFormat.ASCII
-    test = mocked_ff.query_format
-    assert test == ValuesFormat.ASCII
-
-def test_query_fmt_write(mocker, mocked_ff):
-    mocked_ff.query_format = ValuesFormat.BINARY_32
-    mocked_ff.write.assert_called_once_with("FORM2;")
+    assert mocked_ff.frequency==test_f
 
 def test_reset(mocker, mocked_ff):
-    mocked_ff.query.return_value = "TEST"
-    expected_calls = [
-        mocker.call("FACTPRES;"),
-        mocker.call("FORM4;")
-    ]
     mocked_ff.reset()
-    mocked_ff.write.assert_has_calls(expected_calls)
+    mocked_ff.write.assert_called_once_with("FACTPRES;")
 
 def test_wait_until_finished(mocker, mocked_ff):
     mocked_ff.wait_until_finished()
