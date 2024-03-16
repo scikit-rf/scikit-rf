ad339077f6a7daf0bc2f6367737e086555c214fe
Jonathan deWerd <jon@jdewerd.com>
2024-03-16 01:13:54 +0000

    Adding module docs -- does this make class docs populate?

--- skrf/vi/vna/hp/__init__.py
+++ skrf/vi/vna/hp/__init__.py
@@ -1,1 +1,10 @@
+"""
+.. module:: skrf.vi.vna.hp
+===========================================
+HP VNAs (:mod:`skrf.vi.vna.hp`)
+===========================================
+
+From the age when HP made expensive test equipment rather than expensive printer ink.
+
+"""
 from .hp8510c import HP8510C
