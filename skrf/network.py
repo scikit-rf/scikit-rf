a7be2f49f15390b170e1591d196cc5dcd16e45b8
mhuser <mhuser@users.noreply.github.com>
2024-07-13 15:14:46 +0200

    linting

--- skrf/network.py
+++ skrf/network.py
@@ -4710,7 +4710,7 @@ class Network:
 
         if conversion in ["time_impulse", "time_step"]:
             xlabel = "Time (ns)"
-            
+
             t_func_kwargs = {"squeeze": False}
             for key in {"window", "n", "pad", "bandpass"} & kwargs.keys():
                 t_func_kwargs[key] = kwargs.pop(key)
