6ec3e14cac57936638b7789a307182413507c40b
Franz Forstmayr <forstmayr.franz@gmail.com>
2024-08-26 21:04:52 +0200

    linter

--- skrf/tests/test_vectorfitting.py
+++ skrf/tests/test_vectorfitting.py
@@ -1,13 +1,12 @@
-from contextlib import suppress
 import os
 import sys
 import tempfile
 import unittest
+from contextlib import suppress
 from pathlib import Path
 
 import numpy as np
 import pytest
-
 
 with suppress(ImportError):
     from PySpice.Spice.Parser import SpiceParser
