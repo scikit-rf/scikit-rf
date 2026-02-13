d614b4733e03dd8b894adc266cb4ed1c053d9bfc
Franz Forstmayr <forstmayr.franz@gmail.com>
2026-02-13 23:54:05 +0100

    Fix tests

--- skrf/tests/test_util.py
+++ skrf/tests/test_util.py
@@ -7,7 +7,8 @@ class HomoDictTest(unittest.TestCase):
     """
     """
     def setUp(self):
-        self.h = rf.util.HomoDict({'a':'asdf','b':'ZZZZ'})
+        with self.assertWarns(FutureWarning):
+            self.h = rf.util.HomoDict({'a':'asdf','b':'ZZZZ'})
 
     def test_get_item(self):
         self.assertEqual(self.h['a'],'asdf')
@@ -26,7 +27,8 @@ class HomoListTest(unittest.TestCase):
     """
     """
     def setUp(self):
-        self.h = rf.util.HomoList(['asdf','ZZZZ'])
+        with self.assertWarns(FutureWarning):
+            self.h = rf.util.HomoList(['asdf','ZZZZ'])
 
     def test_get_item(self):
         self.assertEqual(self.h[0],'asdf')
