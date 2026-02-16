e6528d5b7b42d9b0e09a424a2b12c40176d1548c
Franz Forstmayr <franz.forstmayr@rosenberger.com>
2026-02-16 16:37:24 +0100

    Fix tests

--- skrf/tests/test_util.py
+++ skrf/tests/test_util.py
@@ -4,44 +4,40 @@ import skrf as rf
 
 
 class HomoDictTest(unittest.TestCase):
-    """
-    """
+    """ """
+
     def setUp(self):
         with self.assertWarns(FutureWarning):
-            self.h = rf.util.HomoDict({'a':'asdf','b':'ZZZZ'})
+            self.h = rf.util.HomoDict({"a": "asdf", "b": "ZZZZ"})
 
     def test_get_item(self):
-        self.assertEqual(self.h['a'],'asdf')
-
+        self.assertEqual(self.h["a"], "asdf")
 
     def test_call(self):
         with self.assertWarns(FutureWarning):
-            self.assertEqual(self.h.upper()['a'],'ASDF')
-
+            self.assertEqual(self.h.upper()["a"], "ASDF")
 
     def test_boolean_mask(self):
         with self.assertWarns(FutureWarning):
-            match_key = [key for key in self.h.keys() if self.h[key].startswith('a') ]
-        self.assertEqual(self.h[match_key], 'asdf')
+            match_key = [key for key in self.h.keys() if self.h[key].startswith("a")]
+            self.assertEqual(self.h[match_key], "asdf")
 
 
 class HomoListTest(unittest.TestCase):
-    """
-    """
+    """ """
+
     def setUp(self):
         with self.assertWarns(FutureWarning):
-            self.h = rf.util.HomoList(['asdf','ZZZZ'])
+            self.h = rf.util.HomoList(["asdf", "ZZZZ"])
 
     def test_get_item(self):
-        self.assertEqual(self.h[0],'asdf')
-
+        self.assertEqual(self.h[0], "asdf")
 
     def test_call(self):
         with self.assertWarns(FutureWarning):
-            self.assertEqual(self.h.upper()[0],'ASDF')
-
+            self.assertEqual(self.h.upper()[0], "ASDF")
 
     def test_boolean_mask(self):
         with self.assertWarns(FutureWarning):
-            match_idx = [idx for idx in range(len(self.h)) if self.h.startswith('a')]
-        self.assertEqual(self.h[match_idx], 'asdf')
+            match_idx = [idx for idx in range(len(self.h)) if self.h.startswith("a")]
+            self.assertEqual(self.h[match_idx], "asdf")
